9101877e5f168f5b3593574390045962953d8707
James Cheung <54218509@qq.com>
2013-06-19 00:06:09 +0800

    设置ECG宽度

--- app/view/EcgDetail.js
+++ app/view/EcgDetail.js
@@ -4,18 +4,18 @@ Ext.define('WebCare.view.EcgDetail', {
   ecgCanvasPref: 'ecgCanvas',
   store: new Ext.data.Store({
     fields: [],
-    data: [{}, {}, {}]
+    data: [{}, {}]
   }),
   columns: [
     {
       text: 'ECG Main',
       dataIndex: '',
-      width: 1280,
+      width: 3050,
       renderer: function(value, p, record, index){
         var html = "";
 
         if (index == 0){
-          html = '<canvas width="1280" height="260" id="' + this.ecgCanvasPref + index + '"></canvas>';
+          html = '<canvas width="3040" height="260" id="' + this.ecgCanvasPref + index + '"></canvas>';
 //                html = "<div style='height: 200; width: 100%'><h1 align='center'>Please Select ECG data</h1></div>";
         }else{
           html = '<div style="height: 200; width: 100%"><h1 align="center">Click to add a ECG for comparison </h1></div>'; // <canvas id="' + ecgCanvasPref + index + '"></canvas>"
